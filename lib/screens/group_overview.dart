94336e4d480045ffc59078e94baa9ae23bf3a5b2
Eeku Koponen <eekukopo@gmail.com>
2020-03-11 17:04:11 +0200

    Added start page

--- lib/screens/group_overview.dart
+++ lib/screens/group_overview.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:t3/widgets/drawer.dart';
+import 'package:provider/provider.dart';
+import 'package:t3/widgets/start.dart';
 
 import '../widgets/groups_grid.dart';
 import '../screens/manage_groups_screen.dart';
+import '../models/members_groups_model.dart';
 
 class GroupOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+   final groupData = Provider.of<MembersGroupsModel>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Groups'),
       ),
-      body: GroupsGrid(),
+      body: groupData.groups.isNotEmpty ? GroupsGrid() : Start(),
       drawer: MainDrawer(),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context)
-                .pushReplacementNamed(ManageGroupsScreen.routeName);
-          },
-          child: Icon(Icons.edit)),
+      floatingActionButton: Visibility( visible: groupData.groups.isNotEmpty ? true : false ,
+              child: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(ManageGroupsScreen.routeName);
+            },
+            child: Icon(Icons.edit)),
+      ),
     );
   }
 }
